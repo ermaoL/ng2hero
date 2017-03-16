20540d0c8f0d0aa01f7853cdbb652e248de1893e
bluewaitor <405622394@qq.com>
2017-03-16 18:01:18 +0800

    修改createdAt字段为created

--- src/app/dashboard/user/user.component.ts
+++ src/app/dashboard/user/user.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from "../../models/User";
-import { UserService } from '../../services/user.service';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../models/User";
+import {UserService} from '../../services/user.service';
 
 @Component({
-    selector: 'user',
-    templateUrl: 'user.component.html',
-    providers: [UserService]
+  selector: 'user',
+  templateUrl: 'user.component.html',
+  providers: [UserService]
 })
 export class UserComponent implements OnInit {
 
-    heroes: User[] = [];
+  heroes: User[] = [];
 
-    constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {
+  }
 
-    ngOnInit() {
-      this.userService.getAllUser().
-      then(heroes => {
-        this.heroes = heroes.slice(0, 10)})
-        .catch((err)=>{
-        });
-    }
+  ngOnInit() {
+    this.userService.getAllUser().then(heroes => {
+      this.heroes = heroes
+    })
+      .catch((err) => {
+      });
+  }
 
 }
