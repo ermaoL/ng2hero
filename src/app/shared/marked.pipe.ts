c71ede96ee343eee0011dbcccdd58c3c074a614e
bluewaitor <405622394@qq.com>
2017-03-17 09:40:41 +0800

    去掉highlight.js

--- src/app/shared/marked.pipe.ts
+++ src/app/shared/marked.pipe.ts
@@ -1,29 +1,16 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 import * as marked from 'marked';
-import {highlightAuto} from 'highlight.js';
+
 @Pipe({
-    name: 'marked'
+  name: 'marked'
 })
 
 export class MarkedPipe implements PipeTransform {
-    transform(value: any, args: any[]): any {
-        if(value) {
-            marked.setOptions({
-                renderer: new marked.Renderer(),
-                gfm: true,
-                tables: true,
-                breaks: false,
-                pedantic: false,
-                sanitize: false,
-                smartLists: true,
-                smartypants: false,
-                highlight: function (code) {
-                    return highlightAuto(code).value;
-                }
-            });
-          return marked(value);
-        }
-        return value;
+  transform(value: any, args: any[]): any {
+    if (value) {
+      return marked(value);
     }
+    return value;
+  }
 }
