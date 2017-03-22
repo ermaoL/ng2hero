1f6e2645b3b6c3ba9da62d6db95b319701943df7
bluewaitor <405622394@qq.com>
2017-03-22 11:31:52 +0800

    视觉优化

--- src/app/dashboard/sider/sider.component.ts
+++ src/app/dashboard/sider/sider.component.ts
@@ -1,25 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from '../../services/auth.service';
-
 @Component({
     selector: 'sider',
     styleUrls: ['sider.component.scss'],
-    templateUrl: 'sider.component.html',
-    providers: [ AuthService ]
+    templateUrl: 'sider.component.html'
 })
 export class SiderComponent implements OnInit {
-    constructor(private _authService: AuthService) { }
+    constructor() { }
 
-    loginUser: Object = null;
     ngOnInit() {
-      if(this._authService.loggedIn()) {
-        this.loginUser = this._authService.getUserInfo().user || {};
-      }
+
     }
 
-    logout(){
-      this._authService.logout();
-    }
 
 }
